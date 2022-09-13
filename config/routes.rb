4abf04a54895d3d7b35a8baac46095911dad7bad
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-13 20:50:54 +0100

    Genrate new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'message/index'
+  get 'static/index'
+  get 'greetings/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 'message#index'
+  root 'greetings#index'
 end
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class MessageControllerTest < ActionDispatch::IntegrationTest
+class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get message_index_url
+    get static_index_url
     assert_response :success
   end
 end
