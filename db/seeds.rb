15b414c70e829d8c7346a428d6f09bd9c87d972c
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-09-24 00:51:40 +0100

    Views and schema update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: 'Hello everyone.')
+Greeting.create(message: 'It\'s nice to see you.')
+Greeting.create(message: 'Welcome.')
+Greeting.create(message: 'Hi!')
+Greeting.create(message: 'Guten Tag.')
--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -1,8 +1,7 @@
 require "test_helper"
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get static_index_url
-    assert_response :success
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
